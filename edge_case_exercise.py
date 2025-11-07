5f8a4d3dc35b5295eb148d1b7c773727062d5a3c
noapeleg <noapeleg2002@gmail.com>
2025-11-07 12:15:56 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,11 +1,9 @@
 def move(my_list, direction):
-
-    # Finds the index of the one in the list
+    my_list = my_list.copy()  # שכפול כדי לא לשנות את הרשימה המקורית
     index_of_one = my_list.index(1)
 
-    # Move the one to the left or to the right
     if direction == 'right':
-        if index_of_one < len(my_list) - 1: 
+        if index_of_one < len(my_list) - 1:
             my_list[index_of_one] = 0
             my_list[index_of_one + 1] = 1
 
